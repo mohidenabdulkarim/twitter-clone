32682acded5260c479f02ff4faf373c7b29ce550
Home <home@Sufis-iMac.local>
2022-05-04 00:53:29 +0300

    wip bmac

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,15 @@
 import Image from 'next/image'
 import React from 'react'
+import { SidebarLink } from '.'
 
 export const Sidebar = () => {
   return (
     <div className="fixed hidden h-full flex-col items-center p-2 sm:flex xl:w-[340px] xl:items-start">
       <div className="hoverAnimation flex h-14 w-14 items-center justify-center p-0 xl:ml-24">
         <Image src="https://rb.gy/ogau5a" width={30} height={30} />
+      </div>
+      <div className="mt-4 mb-2.5 space-y-2.5 xl:ml-24">
+        <SidebarLink />
       </div>
     </div>
   )
